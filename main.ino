318253f38ec4f329fa00dfdc2c8d47f33c560834
paidforby <ggallo102@gmail.com>
2018-09-17 18:40:23 -0700

    added zeroing switch

--- main.ino
+++ main.ino
@@ -14,17 +14,29 @@ volatile byte oldEncPos = 0; //stores the last encoder position value so we can 
 volatile byte reading = 0; //somewhere to store the direct values we read from our interrupt pins before checking to see if we have moved a whole detent
 volatile byte headPos = 8;
 
+int hide_max = 10000;
+int hide_min = 1000;
+
+int show_max = 5000;
+int show_min = 500;
+
 int state = 0;
-int random_hide = random(1000, 10000);
-int random_show = random(1000, 10000);
+int random_hide = random(hide_min, hide_max);
+int random_show = random(show_min, show_max);
 int choice = random(0, 2);
 
 static int motorPinLeft = 5;
 static int motorPinRight = 6;
 
+static int zeroPin = 9; // Our second hardware interrupt pin is digital pin 3
+int ledPin = 13;
+
+
 void setup() {
   pinMode(pinA, INPUT_PULLUP); // set pinA as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
   pinMode(pinB, INPUT_PULLUP); // set pinB as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
+  pinMode(zeroPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
   pinMode(motorPinLeft, OUTPUT);
   pinMode(motorPinRight, OUTPUT);
   attachInterrupt(0,PinA,RISING); // set an interrupt on PinA, looking for a rising edge signal and executing the "PinA" Interrupt Service Routine (below)
@@ -57,40 +69,54 @@ void PinB(){
   sei(); //restart interrupts
 }
 
+long previousTime = millis();
 
 void loop(){
-  //if(oldEncPos != encoderPos) {
+
   Serial.print("Encoder at: ");
   Serial.print(headPos); 
   Serial.print(", State: "); 
-  Serial.println(state);
-    //oldEncPos = encoderPos;
-  //}
-
+  Serial.print(state);
+  int zeroed = digitalRead(zeroPin);
+  /*
+  if( digitalRead(zeroPin) == LOW ){
+    digitalWrite(ledPin, HIGH); 
+    state = 0;
+  }else{
+    digitalWrite(ledPin, LOW); 
+  }
+  */
+  
+  Serial.print(", Zero: "); 
+  Serial.println(zeroed);
+  
   // State 0 - Hide behind Tombstone
   if(state == 0){
     analogWrite(motorPinLeft, 0);
     analogWrite(motorPinRight, 0);
-    //if(millis() - previousTime){
-    Serial.println(choice);
-    delay(random_hide);
-    random_hide = random(2000, 10000);
-    if(choice == 0){
-      state = 1;
-    }else{
-      state = 4;
+    if(millis() - previousTime > random_hide){
+      //choice = 1;
+      Serial.println(choice);
+      //delay(random_hide);
+      random_hide = random(hide_min, hide_max);
+      if(choice == 0){
+        state = 1;
+      }else{
+        state = 4;
+      }
+      choice = random(0, 2);
     }
-    choice = random(0, 2);
   } 
   
   // State 1 - Pop out
   else if(state == 1){
-    if(headPos < 13){
-      analogWrite(motorPinLeft, 255);
-      analogWrite(motorPinRight, 0);
-    }else{
+    analogWrite(motorPinLeft, 255);
+    analogWrite(motorPinRight, 0);
+    // Head fully exposed, exit state
+    if(headPos == 13){
       analogWrite(motorPinLeft, 0);
       analogWrite(motorPinRight, 0);
+      previousTime = millis();
       state++;
     }
   }
@@ -98,58 +124,68 @@ void loop(){
   else if(state == 2){
     analogWrite(motorPinLeft, 0);
     analogWrite(motorPinRight, 0);
-    delay(random_show);
-    random_show = random(1000, 4000);
-    state++;
+    if(millis() - previousTime > random_show){
+      //delay(random_show);
+      // Exit state afer random delay
+      random_show = random(show_min, show_max);
+      state++;
+    }
   }
   // State 3 - Pop back in
   else if(state == 3){
     analogWrite(motorPinLeft, 0);
-    analogWrite(motorPinRight, 255); 
-    if(headPos == 8){
-      analogWrite(motorPinLeft, 0);
-      analogWrite(motorPinRight, 0);
-      //headPos = 0;
-      state=0;
+    analogWrite(motorPinRight, 255);
+    // Head hidden again, return to state 0
+    if( zeroed == LOW ){
+        digitalWrite(ledPin, HIGH); 
+        analogWrite(motorPinLeft, 0);
+        analogWrite(motorPinRight, 0);
+        headPos = 8;
+        previousTime = millis();
+        state=0;
+    }else{
+      digitalWrite(ledPin, LOW); 
     }
   }
   
+  // State 1 - Pop out
   if(state == 4){
     analogWrite(motorPinLeft, 0);
-    analogWrite(motorPinRight, 0);
-    //if(millis() - previousTime){
-    delay(random_hide);
-    random_hide = random(2000, 10000);
-    state++;
-  }
-  
-  // State 1 - Pop out
-  if(state == 5){
-    if(headPos > 3){
-      analogWrite(motorPinLeft, 0);
-      analogWrite(motorPinRight, 255);
-    }else{
+    analogWrite(motorPinRight, 255);
+    // Head fully exposed, exit state
+    if(headPos == 3){
       analogWrite(motorPinLeft, 0);
       analogWrite(motorPinRight, 0);
+      //headPos = 2;
+      previousTime = millis();
       state++;
     }
   }
   // State 2 - Show Goblin Head for X seconds
-  else if(state == 6){
+  else if(state == 5){
     analogWrite(motorPinLeft, 0);
     analogWrite(motorPinRight, 0);
-    delay(random_show);
-    random_show = random(200, 4000);
-    state++;
+    if(millis() - previousTime > random_show){
+      //delay(random_show);
+      // Exit state afer random delay
+      random_show = random(show_min, show_max);
+      state++;
+    }
   }
   // State 3 - Pop back in
-  else if(state == 7){
+  else if(state == 6){
     analogWrite(motorPinLeft, 255);
     analogWrite(motorPinRight, 0);
-    if(headPos == 8){
-      analogWrite(motorPinLeft, 0);
-      analogWrite(motorPinRight, 0);
-      state = 0;
+    // Head hidden again, return to state 0
+    if( zeroed == LOW ){
+        digitalWrite(ledPin, HIGH); 
+        analogWrite(motorPinLeft, 0);
+        analogWrite(motorPinRight, 0);
+        headPos = 8;
+        previousTime = millis();
+        state=0;
+    }else{
+      digitalWrite(ledPin, LOW); 
     }
   }
 }
